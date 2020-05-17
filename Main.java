4bc79d4cdc68873f4d16d04d64be39cf1ef6413f
vnbf <17636688526@163.com>
2020-05-17 18:34:14 +0800

    第一次修改了Main.java并提交到仓库

--- Main.java
+++ Main.java
@@ -10,5 +10,6 @@ public class Main {
         SimpleDateFormat sdf = new SimpleDateFormat("yyyy-MM-dd");
         System.out.println("当前的日期："+"2020-05-01");
         System.out.println("下一天日期："+nextDate);
+		System.out.println("明 天日期："+nextDate);
     }
 }
